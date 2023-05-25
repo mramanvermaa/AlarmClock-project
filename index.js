3e3baba184241faa3511b60be34fb3c7904f5e72
Amanvrma7879 <133656778+mramanvermaa@users.noreply.github.com>
2023-05-25 13:02:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,133 @@
+const display = document.getElementById('clock');
+
+// audio sound set for ringing the alarm
+
+const audio = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-alarm-digital-clock-beep-989.mp3');
+audio.loop = true;
+
+
+let alarmTime = null;
+let alarmTimeout = null;
+
+
+const myList = document.querySelector('#myList');
+const addAlarm = document.querySelector('.setAlarm')
+
+
+const alarmList = [];  //  --- store upcoming alarm user wants to set
+
+
+// at the correct time audio play
+
+
+function ringing(now){
+    audio.play();
+    alert(`Hey! it is ${now}`)
+}
+
+// the time updates every second
+
+function updateTime() {
+    var today = new Date();
+    const hour = formatTime(today.getHours());
+    const minutes = formatTime(today.getMinutes());
+    const seconds = formatTime(today.getSeconds());
+    const now = `${hour}:${minutes}:${seconds}`;
+
+    display.innerText=`${hour}:${minutes}:${seconds}`;
+    
+//     check if the alarmList includes the current time , "now"
+//     if yes, ringing() is called
+    if(alarmList.includes(now) ){
+        ringing(now);
+    } 
+}
+
+
+// set the correct format of time
+// converts "1:2:3" to "01:02:03"
+function formatTime(time) {
+    if ( time < 10 && time.length != 2) {
+        return '0' + time;
+    }
+    return time;
+}
+
+// this is function to pause the currently playing the alarm 
+
+function clearAlarm() {
+    audio.pause();
+    if (alarmTimeout) {
+        clearTimeout(alarmTimeout);
+        alert('Alarm cleared');
+    }
+}      
+
+// this removes an ul when clicked on the delete alarm button
+
+myList.addEventListener('click', e=> {
+    console.log("removing element")
+    if(e.target.classList.contains("deleteAlarm")){
+        e.target.parentElement.remove();
+    }    
+})
+
+// this remove the alarm from the array the 'delete Alarm ' button is clicked
+
+remove = (value) => {
+    let newList = alarmList.filter((time) => time != value);
+    alarmList.length = 0;                  // Clear contents
+    alarmList.push.apply(alarmList, newList);
+    
+    console.log("newList", newList);
+    console.log("alarmList", alarmList);
+}
+
+// this create/add new alarm to the ul
+
+function showNewAlarm(newAlarm){
+    const html =`
+    <li class = "time-list">        
+        <span class="time">${newAlarm}</span>
+        <button class="deleteAlarm time-control" id="delete-button" onclick = "remove(this.value)" value=${newAlarm}>Delete Alarm</button>       
+    </li>`
+    myList.innerHTML += html
+};
+
+addAlarm.addEventListener('submit', e=> {
+    e.preventDefault();
+    
+    let new_h=formatTime(addAlarm.a_hour.value);
+    if(new_h === '0'){
+        new_h = '00'
+    }
+    let new_m=formatTime(addAlarm.a_min.value);
+    if(new_m === '0'){
+        new_m = '00'
+    }
+    let new_s=formatTime(addAlarm.a_sec.value);
+    if(new_s === '0'){
+        new_s = '00'
+    }
+    
+    const newAlarm = `${new_h}:${new_m}:${new_s}`
+
+//  this add newAlarm to alarmList
+    if(isNaN(newAlarm)){
+        if(!alarmList.includes(newAlarm)){
+            alarmList.push(newAlarm);
+            console.log(alarmList);
+            console.log(alarmList.length);
+            showNewAlarm(newAlarm);
+            addAlarm.reset();
+        } else{
+            alert(`Alarm for ${newAlarm} already set.`);
+        }
+    } else{
+        alert("Invalid Time Entered")
+    }        
+})
+
+
+// this is for calls updateTime() in every second
+setInterval(updateTime, 1000);
